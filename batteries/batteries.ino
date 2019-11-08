174c6fdb14e4e4e72e21c194c5eb75222e38103b
Francisco Troncoso <ftroncoso@uvigo.es>
2019-11-08 11:21:07 +0100

    Added extraction of battery state with two possible configurations, refactor.
    Added and modified battery state extraction

---
+++ batteries/batteries.ino
@@ -0,0 +1,190 @@
+/**
+  batteries.ino: Manage battery connection, LED updates and state transmision.
+
+  The LEDs have the following meaning:
+  - Green color: Battery is fully charged
+  - Amber color: Battery is half charged
+  - Red color:   Battery is uncharged
+  - Blink state: Battery is in use
+  - Fixed state: Battery is not in use
+
+  The state is sent through the serial interface using two possible methods:
+  - The following binary dataframe (5 bytes):
+      +--------+-------+------+------+-----+
+      | HEADER | FLAGS | BT1V | BT2V | CHK |
+      +--------+-------+------+------+-----+
+      - HEADER:    Constant value: 0xFE
+      - FLAGS:     Bit 2: battery 1 connected, bit 0: battery 2 connected
+      - BT1V/BT2V: Battery 1/2 voltage
+      - CHK:       Checksum
+  - The following text dataframe (18 characters):
+    @A<C1><V1>$@B<C2><V2>$
+      - C1/C2: Battery 1/2 connected (1 character)
+      - V1/V2: Battery 1/2 voltage, in mV (5 characters)
+*/
+
+
+// ****************************************************************************
+// Constants
+// ****************************************************************************
+
+const bool BINARY_MODE          =  true;
+
+// Pins
+const byte BT1_RELAY_ON_PIN     =     2;
+const byte BT1_RELAY_OFF_PIN    =     3;
+const byte BT2_RELAY_ON_PIN     =     6;
+const byte BT2_RELAY_OFF_PIN    =     7;
+const byte BT1_GREEN_LED_PIN    =     4;
+const byte BT1_RED_LED_PIN      =     5;
+const byte BT2_GREEN_LED_PIN    =     8;
+const byte BT2_RED_LED_PIN      =     9;
+const byte BT1_VOLTAGE_PIN      =    A0;
+const byte BT2_VOLTAGE_PIN      =    A1;
+const byte BT1_STATE_PIN        =    A6;
+const byte BT2_STATE_PIN        =    A7;
+
+// Thresholds
+const word BATTERY_CONNECTED_TH =   200;
+const word BATTERY_HIGH_TH      =   120;  // dV
+const word BATTERY_MEDIUM_TH    =   115;  // dV
+const word BATTERY_LOW_TH       =   110;  // dV
+
+// Serial
+const byte MSG_HEADER           =  0xFE;
+
+
+// ****************************************************************************
+// Structures
+// ****************************************************************************
+
+struct State {
+  byte bt1Voltage, bt2Voltage;  // dV
+  bool bt1Connected, bt2Connected;
+};
+
+
+// ****************************************************************************
+// Variables
+// ****************************************************************************
+
+State state = { 0, 0, 0, 0 };
+bool blink = false;
+
+
+// ****************************************************************************
+// Main
+// ****************************************************************************
+
+// ----------------------------------------------------------------------------
+// Setup method
+void setup() 
+{
+  // Pins
+  pinMode(BT1_RELAY_ON_PIN , OUTPUT);
+  pinMode(BT1_RELAY_OFF_PIN, OUTPUT);
+  pinMode(BT2_RELAY_ON_PIN , OUTPUT);
+  pinMode(BT2_RELAY_OFF_PIN, OUTPUT);
+  pinMode(BT1_GREEN_LED_PIN, OUTPUT);
+  pinMode(BT1_RED_LED_PIN  , OUTPUT);
+  pinMode(BT2_GREEN_LED_PIN, OUTPUT);
+  pinMode(BT2_RED_LED_PIN  , OUTPUT);
+  digitalWrite(BT1_RELAY_ON_PIN , HIGH);
+  digitalWrite(BT1_RELAY_OFF_PIN, HIGH);
+  digitalWrite(BT2_RELAY_ON_PIN , HIGH);
+  digitalWrite(BT2_RELAY_OFF_PIN, HIGH);
+  digitalWrite(BT1_GREEN_LED_PIN, LOW );  
+  digitalWrite(BT1_RED_LED_PIN  , LOW );
+  digitalWrite(BT2_GREEN_LED_PIN, LOW );
+  digitalWrite(BT2_RED_LED_PIN  , LOW );
+
+  // Serial
+  Serial.begin(9600);
+
+  delay(2000);
+}
+
+// ----------------------------------------------------------------------------
+// Loop method
+void loop() 
+{
+  // Get state
+  state.bt1Voltage   = (word)analogRead(BT1_VOLTAGE_PIN) * 10 / 73;
+  state.bt2Voltage   = (word)analogRead(BT2_VOLTAGE_PIN) * 10 / 73;
+  state.bt1Connected = (word)analogRead(BT1_STATE_PIN) >= BATTERY_CONNECTED_TH;
+  state.bt2Connected = (word)analogRead(BT2_STATE_PIN) >= BATTERY_CONNECTED_TH;
+
+  // Update battery connections
+  // If none connected, connect the first one that is charged
+  if (!state.bt1Connected && !state.bt2Connected) {
+    if        (state.bt1Voltage >= BATTERY_LOW_TH) {
+      switchBattery(BT1_RELAY_ON_PIN, BT2_RELAY_OFF_PIN);
+    } else if (state.bt1Voltage >= BATTERY_LOW_TH) {
+      switchBattery(BT2_RELAY_ON_PIN, BT1_RELAY_OFF_PIN);
+    }
+  // If battery 1 low, switch to battery 2
+  } else if (state.bt1Connected
+          && state.bt1Voltage <  BATTERY_LOW_TH
+          && state.bt2Voltage >= BATTERY_LOW_TH) {
+    switchBattery(BT2_RELAY_ON_PIN, BT1_RELAY_OFF_PIN);
+  // If battery 2 low, switch to battery 1
+  } else if (state.bt2Connected
+          && state.bt2Voltage <  BATTERY_LOW_TH
+          && state.bt1Voltage >= BATTERY_LOW_TH) {
+    switchBattery(BT1_RELAY_ON_PIN, BT2_RELAY_OFF_PIN);
+  }
+
+  // Update LED state
+  blink = !blink;
+  updateLeds(state.bt1Connected, state.bt1Voltage,
+             BT1_GREEN_LED_PIN, BT1_RED_LED_PIN);
+  updateLeds(state.bt2Connected, state.bt2Voltage,
+             BT2_GREEN_LED_PIN, BT2_RED_LED_PIN);
+
+  // Send state
+  if (BINARY_MODE) {
+    // - PAYLOAD: | HEADER | FLAGS | BT1V | BT2V | CHK |
+    byte msg[] = {
+      MSG_HEADER,
+      (byte)(state.bt1Connected << 2 | state.bt2Connected << 0),
+      state.bt1Voltage, state.bt2Voltage, 0
+    };
+    for (byte i = 1; i < 4; i++) msg[4] += msg[i];
+    msg[4] = 0xFF - msg[4] + 1;
+    Serial.write(msg, 5);
+  } else {
+    char serialOutBuffer[15];
+    sprintf(serialOutBuffer, "@A%1d%03d$@B%1d%03d$",
+      state.bt1Connected, state.bt1Voltage,
+      state.bt2Connected, state.bt2Voltage);
+    Serial.println(serialOutBuffer);
+  }
+
+  delay(500);
+}
+
+
+// ****************************************************************************
+// Helpers
+// ****************************************************************************
+
+// ----------------------------------------------------------------------------
+// Battery connection handling
+void switchBattery(byte onPin, byte offPin) {
+  digitalWrite(onPin , LOW );
+  delay(200);
+  digitalWrite(onPin , HIGH);
+  delay(200);
+  digitalWrite(offPin, LOW );
+  delay(200);
+  digitalWrite(offPin, HIGH);
+}
+
+
+// ----------------------------------------------------------------------------
+// LED handling
+void updateLeds(bool connected, word voltage, byte greenPin, byte redPin) {
+  const auto value = connected || blink ? HIGH : LOW;
+  digitalWrite(greenPin, voltage >= BATTERY_MEDIUM_TH ? value : LOW);
+  digitalWrite(redPin  , voltage <  BATTERY_HIGH_TH   ? value : LOW);
+}
--- motors/motors.ino
+++ motors/motors.ino
@@ -30,28 +30,28 @@
 
   I2C
   ================
-  Two possible byte dataframes:
+  Two possible binary dataframes (big-endian):
     - Move (6 bytes): Updates the target position for the three motors at the
       same time:
-        +----------+--------+--------+-----+------+-----+
-        | CMD_MOVE | VERT_H | VERT_L | PAN | TILT | CHK |
-        +----------+--------+--------+-----+------+-----+
+        +----------+----+----+-----+------+-----+
+        | CMD_MOVE |  VERT   | PAN | TILT | CHK |
+        +----------+----+----+-----+------+-----+
     - Read (1 byte): Requests the current state of the system:
         +----------+
         | CMD_READ |
         +----------+
       The return dataframe is 8 bytes long:
-        +--------+--------+-----+------+-------+------+------+-----+
-        | VERT_H | VERT_L | PAN | TILT | FLAGS | BT1V | BT2V | CHK |
-        +--------+--------+-----+------+-------+------+------+-----+
+        +----+----+-----+------+-------+------+------+-----+
+        |  VERT   | PAN | TILT | FLAGS | BT1V | BT2V | CHK |
+        +----+----+-----+------+-------+------+------+-----+
       The "flags" byte has the following contents:
         +-----+-----+-----+-----+-----+-----+-----+-----+
         | MFT | MST | ULS | LLS |    BT1    |    BT2    |
         +-----+-----+-----+-----+-----+-----+-----+-----+
-        - MFT:      Motor fault
-        - MST:      Motor stopped
-        - ULS/LLS:  Upper/Lower limit switch pressed
-        - BT1/BT2:  Battery 1/2 state
+        - MFT:        Motor fault
+        - MST:        Motor stopped
+        - ULS/LLS:    Upper/Lower limit switch pressed
+        - BT1V/BT2V:  Battery 1/2 voltage
 */
 
 #include <Servo.h>
@@ -62,46 +62,51 @@
 // Constants
 // ****************************************************************************
 
+const bool BINARY_MODE            =  true;
+
 // Pins
-const byte ENCODER_A_PIN          =    2;
-const byte ENCODER_B_PIN          =    3;
-const byte ENCODER_INT_PIN        =    2;
-const byte MOTOR_PWM_PIN          =    6;
-const byte MOTOR_DIR_PIN          =    7;
-const byte MOTOR_FBK_PIN          =   A0;
-const byte MOTOR_ND2_PIN          =    4;
-const byte MOTOR_NSF_PIN          =   12;
-const byte SERVO_PAN_PIN          =   11;
-const byte SERVO_PAN_FBK_PIN      =   A2;
-const byte SERVO_TILT_PIN         =   10;
-const byte SERVO_TILT_FBK_PIN     =   A1;
-const byte LOWER_LIMIT_SWITCH_PIN =   A6;
-const byte UPPER_LIMIT_SWITCH_PIN =   A7;
+const byte ENCODER_A_PIN          =     2;
+const byte ENCODER_B_PIN          =     3;
+const byte ENCODER_INT_PIN        =     2;
+const byte MOTOR_PWM_PIN          =     6;
+const byte MOTOR_DIR_PIN          =     7;
+const byte MOTOR_FBK_PIN          =    A0;
+const byte MOTOR_ND2_PIN          =     4;
+const byte MOTOR_NSF_PIN          =    12;
+const byte SERVO_PAN_PIN          =    11;
+const byte SERVO_PAN_FBK_PIN      =    A2;
+const byte SERVO_TILT_PIN         =    10;
+const byte SERVO_TILT_FBK_PIN     =    A1;
+const byte LOWER_LIMIT_SWITCH_PIN =    A6;
+const byte UPPER_LIMIT_SWITCH_PIN =    A7;
+const byte WIRELESS_PIN           =     9;
 
 // Communication
-const byte I2C_ADDR               = 0x16;
-const char SERIAL_BGN_MARKER      = '@';
-const char SERIAL_END_MARKER      = '$';
+const byte I2C_ADDR               =  0x16;
+const char SERIAL_BGN_MARKER      =   '@';
+const char SERIAL_END_MARKER      =   '$';
 
 // Commands
-const byte CMD_MOVE               = 0x4D;  // 'M'
-const byte CMD_READ               = 0x52;  // 'R'
+const byte CMD_MOVE               =  0x4D;  // 'M'
+const byte CMD_READ               =  0x52;  // 'R'
 
 // Motors
-const word MOTOR_MM_STEPS         =   25;
-const byte MOTOR_SPEED            =  127;
-const byte MOTOR_FBK_TH           =  110;
-const word LIMIT_SWITCH_TH        =  400;
+const word MOTOR_MM_STEPS         =    25;
+const byte MOTOR_SPEED            =   127;
+const byte MOTOR_FBK_TH           =   110;
+const word LIMIT_SWITCH_TH        =   400;
 const word MAX_VERT_STEPS         = 0xFFFF - MOTOR_MM_STEPS;
 
 // Flags
-const byte FLAG_LOWER_LIMIT       =    4;
-const byte FLAG_UPPER_LIMIT       =    5;
-const byte FLAG_MOTOR_STOP        =    6;
-const byte FLAG_MOTOR_FAULT       =    7;
+const byte FLAG_BT1STATE          =     0;
+const byte FLAG_BT2STATE          =     2;
+const byte FLAG_LOWER_LIMIT       =     4;
+const byte FLAG_UPPER_LIMIT       =     5;
+const byte FLAG_MOTOR_STOP        =     6;
+const byte FLAG_MOTOR_FAULT       =     7;
 
 // Other
-const word LOOP_DELAY             =   10;  // ms
+const word LOOP_DELAY             =    10;  // ms
 
 
 // ****************************************************************************
@@ -110,7 +115,7 @@ const word LOOP_DELAY             =   10;  // ms
 
 struct State {
   word vert;
-  byte pan, tilt, flags, bat1Voltage, bat2Voltage;
+  byte pan, tilt, flags, bt1Voltage, bt2Voltage;
 };
 
 struct Target {
@@ -125,11 +130,13 @@ struct Target {
 
 // Motors
 Servo motorPan, motorTilt;
+
 // Context
 volatile byte cmd = CMD_READ;
 volatile word vert_steps = 0;
 volatile State state = { 0, 0, 0, 0, 0, 0 };
 volatile Target target = { 0, 0, 0 };
+
 // Serial
 char serialInBuffer[8];
 byte serialInIdx = 0;
@@ -146,16 +153,18 @@ byte serialOutIdx = 0;
 // Setup method
 void setup() {
   // Pins
-  pinMode(ENCODER_A_PIN, INPUT);
-  digitalWrite(ENCODER_A_PIN, HIGH);
-  pinMode(ENCODER_B_PIN, INPUT);
-  digitalWrite(ENCODER_B_PIN, HIGH);
+  pinMode(ENCODER_A_PIN, INPUT );
+  pinMode(ENCODER_B_PIN, INPUT );
   pinMode(MOTOR_PWM_PIN, OUTPUT);
   pinMode(MOTOR_DIR_PIN, OUTPUT);
-  pinMode(MOTOR_FBK_PIN, INPUT);
+  pinMode(MOTOR_FBK_PIN, INPUT );
   pinMode(MOTOR_ND2_PIN, OUTPUT);
+  pinMode(MOTOR_NSF_PIN, INPUT );
+  pinMode(WIRELESS_PIN , OUTPUT);
+  digitalWrite(ENCODER_A_PIN, HIGH);
+  digitalWrite(ENCODER_B_PIN, HIGH);
   digitalWrite(MOTOR_ND2_PIN, HIGH);
-  pinMode(MOTOR_NSF_PIN, INPUT);
+  digitalWrite(WIRELESS_PIN , HIGH);
 
   // Interrupts
   attachInterrupt(digitalPinToInterrupt(ENCODER_INT_PIN),
@@ -171,6 +180,8 @@ void setup() {
   Wire.onRequest(requestEvent);
   Serial.begin(9600);
 
+  delay(2000);
+
   // Move motors to position that is read from feedback
   const byte panPos  = analogRead(SERVO_PAN_FBK_PIN ) * 10 / 42;
   const byte tiltPos = analogRead(SERVO_TILT_FBK_PIN) * 10 / 42;
@@ -181,7 +192,11 @@ void setup() {
 // ----------------------------------------------------------------------------
 // Loop method
 void loop() {
-  readSerial();
+  if (BINARY_MODE) {
+    readBatteryState();
+  } else {
+    readSerial();
+  }
   noInterrupts();
   state.vert = (vert_steps + MOTOR_MM_STEPS / 2) / MOTOR_MM_STEPS;
   word state_vert  = state.vert;
@@ -212,14 +227,14 @@ void updateMotor(word current, word target) {
   // - Motor fault detected
   if (!digitalRead(MOTOR_NSF_PIN)) {
     dir = 0; // Stop motion
-    bitSet(state.flags, FLAG_MOTOR_FAULT);
+    bitSet  (state.flags, FLAG_MOTOR_FAULT);
   } else {
     bitClear(state.flags, FLAG_MOTOR_FAULT);
   }
   // - Motor feedback current too high
   if (analogRead(MOTOR_FBK_PIN) > MOTOR_FBK_TH) {
     dir = 0; // Stop motion
-    bitSet(state.flags, FLAG_MOTOR_STOP);
+    bitSet  (state.flags, FLAG_MOTOR_STOP);
   } else {
     bitClear(state.flags, FLAG_MOTOR_STOP);
   }
@@ -229,14 +244,14 @@ void updateMotor(word current, word target) {
     noInterrupts();
     vert_steps = 0;
     interrupts();
-    bitSet(state.flags, FLAG_LOWER_LIMIT);
+    bitSet  (state.flags, FLAG_LOWER_LIMIT);
   } else {
     bitClear(state.flags, FLAG_LOWER_LIMIT);
   }
   // - Upper limit switch pressed
   if (analogRead(UPPER_LIMIT_SWITCH_PIN) < LIMIT_SWITCH_TH) {
     if (dir > 0) dir = 0; // Stop up motion
-    bitSet(state.flags, FLAG_UPPER_LIMIT);
+    bitSet  (state.flags, FLAG_UPPER_LIMIT);
   } else {
     bitClear(state.flags, FLAG_UPPER_LIMIT);
   }
@@ -292,13 +307,13 @@ void receiveEvent(int numBytes) {
         Serial.println("\nERROR: Wrong number of bytes");
         break;
       }
-      byte vert_h   = Wire.read();
-      byte vert_l   = Wire.read();
-      byte pan      = Wire.read();
-      byte tilt     = Wire.read();
-      byte checksum = Wire.read();
+      const byte vert_h = Wire.read();
+      const byte vert_l = Wire.read();
+      const byte pan    = Wire.read();
+      const byte tilt   = Wire.read();
+      const byte chk    = Wire.read();
       read += 5;
-      byte sum = vert_h + vert_l + pan + tilt + checksum;
+      const byte sum = vert_h + vert_l + pan + tilt + chk;
       if (sum != 0) {
         Serial.println("\nERROR: Incorrect checksum");
         break;
@@ -331,13 +346,37 @@ void requestEvent() {
   // - PAYLOAD: | VERT_H | VERT_L | PAN | TILT | FLAGS | BT1V | BT2V | CHK |
   byte msg[] = {
     (byte)(state.vert >> 8), (byte)state.vert, state.pan, state.tilt,
-    state.flags, state.bat1Voltage, state.bat2Voltage, 0
+    state.flags, state.bt1Voltage, state.bt2Voltage, 0
   };
   for (byte i = 0; i < 7; i++) msg[7] += msg[i];
   msg[7] = 0xFF - msg[7] + 1;
 
   // Send
   Wire.write(msg, 8);
+}
+
+// ----------------------------------------------------------------------------
+// Reads the state of the batteries from the wireless serial module
+void readBatteryState() {
+  // - PAYLOAD: | HEADER | BT1V | BT2V | CHK |
+  const byte MSG_HEADER = 0xFE;
+  while (Serial.available() && Serial.read() != MSG_HEADER);
+  if (Serial.available() < 5) return;
+  const byte flags = Serial.read();
+  const byte bt1v  = Serial.read();
+  const byte bt2v  = Serial.read();
+  const byte chk   = Serial.read();
+  const byte sum = flags + bt1v + bt2v + chk;
+  if (sum != 0) {
+    Serial.println("\nERROR: Incorrect checksum");
+    return;
+  }
+  if (flags & FLAG_BT1STATE) bitSet  (state.flags, FLAG_BT1STATE);
+  else                       bitClear(state.flags, FLAG_BT1STATE);
+  if (flags & FLAG_BT2STATE) bitSet  (state.flags, FLAG_BT2STATE);
+  else                       bitClear(state.flags, FLAG_BT2STATE);
+  state.bt1Voltage = bt1v;
+  state.bt2Voltage = bt2v;
 }
 
 // ----------------------------------------------------------------------------
@@ -356,14 +395,14 @@ void readSerial() {
         serialInBuffer[serialInIdx++] = ch;
       } else {
         // - End
-        serialInBuffer[serialInIdx++] = '\0';
+        serialInBuffer[serialInIdx] = '\0';
         processSerialCommand(serialInBuffer);
         serialInStarted = false;
       }
     }
 
     // Check command length
-    if (serialInIdx == 7) {
+    if (serialInIdx == 8) {
       Serial.print("\nERROR: Command too long");
       serialInStarted = false;
     }
@@ -373,11 +412,22 @@ void readSerial() {
 // ----------------------------------------------------------------------------
 // Process a command sent through the serial interface
 void processSerialCommand(const char* cmd) {
-  word value = atoi(cmd + 1);
+  const unsigned long TH = 1000;
+  const unsigned long value = atol(cmd + 1);
   switch (cmd[0]) {
     case 'V': target.vert = value; return;
     case 'P': target.pan  = value; return;
     case 'T': target.tilt = value; return;
+    case 'A':
+      state.bt1Voltage = value % TH;
+      if (value >= TH) bitSet  (state.flags, FLAG_BT1STATE);
+      else             bitClear(state.flags, FLAG_BT1STATE);
+      return;
+    case 'B':
+      state.bt2Voltage = value % TH;
+      if (value >= TH) bitSet  (state.flags, FLAG_BT2STATE);
+      else             bitClear(state.flags, FLAG_BT2STATE);
+      return;
   }
   Serial.print("\nERROR: Unknown command ");
   Serial.println(cmd[0]);
@@ -397,8 +447,8 @@ void writeSerial() {
       target.vert, target.pan, target.tilt,
       state.vert , state.pan , state.tilt ,
       state.flags,
-      state.bat1Voltage / 10, state.bat1Voltage % 10,
-      state.bat2Voltage / 10, state.bat2Voltage % 10);
+      state.bt1Voltage / 10, state.bt1Voltage % 10,
+      state.bt2Voltage / 10, state.bt2Voltage % 10);
   }
 
   // Print one character at a time
